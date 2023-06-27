0845efe02c041d7cf1871d87f88b820851f620eb
Anastasia <menshikova.naya@mail.ru>
2023-06-27 12:44:02 +0500

    Автотесты 2 после ревью

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const { createCard, getCards, deleteCard, putLike, dislikeCard } = require('../controllers/cards');
+const {
+  createCard,
+  getCards,
+  deleteCard,
+  putLike,
+  dislikeCard,
+} = require("../controllers/cards");
 
-router.post('/', createCard);
-router.get('/', getCards);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', putLike)
-router.delete('/:cardId/likes', dislikeCard)
+router.post("/", createCard);
+router.get("/", getCards);
+router.delete("/:cardId", deleteCard);
+router.put("/:cardId/likes", putLike);
+router.delete("/:cardId/likes", dislikeCard);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const { getUsers, createUser, getUserById, updateUser, updateAvatar } = require('../controllers/users');
+const {
+  getUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  updateAvatar,
+} = require("../controllers/users");
 
-router.post('/', createUser);
-router.get('/', getUsers);
-router.get('/:userId', getUserById);
-router.patch('/me', updateUser)
-router.patch('/me/avatar', updateAvatar)
-
+router.post("/", createUser);
+router.get("/", getUsers);
+router.get("/:userId", getUserById);
+router.patch("/me", updateUser);
+router.patch("/me/avatar", updateAvatar);
 
 module.exports = router;
