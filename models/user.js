15b16aa40dbdd6d0ffadc8d4a5d95c661c3ab0f3
Anastasia <menshikova.naya@mail.ru>
2023-07-04 12:51:52 +0500

    Автотетсы 4

--- models/user.js
+++ models/user.js
@@ -4,28 +4,29 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Используйте корректную ссылку',
+    },
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    validator: {
+    validate: {
       validator: (v) => validator.isEmail(v),
       message: 'Укажите верный e-mail',
     },
