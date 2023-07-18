78af725e78013991b67c0116147b7591914851ff
Anastasia <menshikova.naya@mail.ru>
2023-07-18 11:50:55 +0500

    на сервер

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
+require('dotenv').config();
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const router = require('./routes');
 
 const errorHandler = require('./middlewares/error-handler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(cors);
 // анализирует входящие запросы JSON и помещает проанализированные данные в файлы req.body
 app.use(express.json());
+app.use(requestLogger);
 app.use(cookieParser());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(router);
+app.use(errorLogger);
 app.use(errors());
 
 app.use(errorHandler);
