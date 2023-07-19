cbb9b59047978b4b42f3ae1d687012e0e9c7022d
Anastasia <menshikova.naya@mail.ru>
2023-07-19 16:47:55 +0500

    no-cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cookieParser = require('cookie-parser');
-// const cors = require('cors');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const router = require('./routes');
@@ -16,32 +16,10 @@ const { PORT, MONGO_DB } = require('./utils/constant');
 
 const app = express();
 
-const allowedCors = [
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'https://lolewka.domainname.studen.nomoredomains.xyz',
-  'http://lolewka.domainname.studen.nomoredomains.xyz',
-];
-
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-const corsHandler = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
-  }
-  next();
-};
-
-app.use(corsHandler);
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://lolewka.domainname.studen.nomoredomains.xyz', 'http://lolewka.domainname.studen.nomoredomains.xyz'],
+  mode: 'no-cors',
+}));
 // анализирует входящие запросы JSON и помещает проанализированные данные в файлы req.body
 app.use(express.json());
 app.use(requestLogger);
