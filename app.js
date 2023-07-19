8943f00d9e42d77c2a7af3313416673c2132237b
Anastasia <menshikova.naya@mail.ru>
2023-07-19 16:20:51 +0500

    cors

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ const { PORT, MONGO_DB } = require('./utils/constant');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://lolewka.domainname.studen.nomoredomains.xyz', 'http://lolewka.domainname.studen.nomoredomains.xyz'],
+}));
 // анализирует входящие запросы JSON и помещает проанализированные данные в файлы req.body
 app.use(express.json());
 app.use(requestLogger);
